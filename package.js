e7ce318a119988a3bd8fde8441681d9207397acf
Chris Waring <chris@wwaves.co>
2015-03-28 16:39:06 -0400

    add git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Transpile CSS4 using Rework',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cwaring/meteor-css4',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
