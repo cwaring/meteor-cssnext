9c82de6bc9641c6568280b6627ad22194a251d57
Chris Waring <chris@wwaves.co>
2015-03-28 16:39:39 -0400

    rm comments

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'kit:css4',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Transpile CSS4 using Rework',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/cwaring/meteor-css4',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
