da65a993bc656c54f4fc19faa35435f5e16b4d1c
Chris Waring <chris@wwaves.co>
2015-04-01 12:24:41 -0400

    depend on basswork 1.4.0

--- package.js
+++ package.js
@@ -18,15 +18,6 @@ Package.registerBuildPlugin({
   'plugin/css4.js'
   ],
   npmDependencies: {
-    "autoprefixer": "5.0.0",
-    "minimist": "1.1.0",
-    "rework": "1.0.1",
-    "rework-calc": "1.1.0",
-    "rework-color-function": "1.2.1",
-    "rework-custom-media": "0.2.0",
-    "rework-namespace": "0.3.0",
-    "rework-npm": "1.0.0",
-    "rework-plugin-colors": "1.0.3",
-    "rework-vars": "3.1.1"
+    "basswork": "1.4.0"
   }
 });
--- plugin/css4.js
+++ plugin/css4.js
@@ -1,33 +1,4 @@
-var rework = Npm.require('rework');
-var reworkCalc = Npm.require('rework-calc');
-var reworkMedia = Npm.require('rework-custom-media');
-var reworkNPM = Npm.require('rework-npm');
-var reworkVars = Npm.require('rework-vars');
-var reworkColor = Npm.require('rework-color-function');
-var reworkPluginColors = Npm.require('rework-plugin-colors');
-var namespace = Npm.require('rework-namespace');
-var autoprefixer = Npm.require('autoprefixer');
-
-var basswork = function(src, options, toStringOptions) {
-
-  var options = options || {};
-  var toStringOptions = toStringOptions || {};
-
-  var css = rework(src, options)
-    .use(reworkNPM())
-    .use(reworkVars())
-    .use(reworkMedia())
-    .use(reworkCalc)
-    .use(reworkColor)
-    .use(reworkPluginColors()) // For legacy support
-    .use(namespace(options.namespace, options.namespaceOptions))
-    .toString(toStringOptions);
-
-  css.code = autoprefixer().process(css.code.toString()).css;
-
-  return css;
-
-};
+var basswork = Npm.require('basswork')
 
 var handler = function (compileStep, isLiterate) {
 
